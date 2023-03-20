76e474a019cb6f3f84518705c124f5f7e6fdd51a
Gregory Ross <gregross.dev@gmail.com>
2023-03-20 17:06:48 -0400

    comment out vite

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+{{--    @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
 </head>
 <body class="font-sans antialiased">
     <div class="site-wrapper">
