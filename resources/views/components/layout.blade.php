e3b39b71b6055f8adab69963ac21f1222e58952f
Gregory Ross <gregross.dev@gmail.com>
2023-03-21 10:12:23 -0400

    again

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,9 +6,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+{{--    <link rel="stylesheet" href="{{ asset('build/assets/app-db279ac8.css') }}">--}}
 
-{{--    @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased">
     <div class="site-wrapper">
