913ea2b458e1eb3a957d85c3343357f50768a780
Gregory Ross <gregross.dev@gmail.com>
2023-03-21 11:00:01 -0400

    yet again

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,7 +1,7 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="grid max-w-screen-xl px-4 sm:px-6 lg:px-8 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto lg:col-span-7">
-            <h1 class="max-w-xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Recovery Housing that works!!!</h1>
+            <h1 class="max-w-xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Recovery Housing that works!!! When you work it.</h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From treatment plans to long term recovery goals.</p>
 
 
