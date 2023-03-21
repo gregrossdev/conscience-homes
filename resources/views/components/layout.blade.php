e49be463e000785a210148d70a57d682538bcffe
Gregory Ross <gregross.dev@gmail.com>
2023-03-21 08:19:26 -0400

    link tailwindcss

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 {{--    @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
 </head>
