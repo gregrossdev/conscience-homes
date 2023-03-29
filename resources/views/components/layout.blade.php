cc0c69d9b5eed1916e9387dfdd759115d1851863
Gregory Ross <gregross.dev@gmail.com>
2023-03-29 16:38:02 -0400

    update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 <body class="font-sans antialiased">
     <div class="site-wrapper">
